3b06c23a29ba3d76a2c24b790e9f3027cfd2b796
Shin Seongjoo <tloc0e04@osstem.com>
2023-01-12 16:54:29 +0900

    1065

--- 1065.py
+++ 1065.py
@@ -3,14 +3,13 @@ N = int(input())
 ans = 0
 if N < 100:
 	ans = N
-	print(ans)
 else:
 	ans = 99
-	for i in range(110, N+1):
+	for i in range(100, N+1):
 		a = i // 100
 		b = (i - (a*100)) // 10
 		c = (i - (a*100) - (b*10))
-		if (a + c) / 2 == b:
+		if (a-b) == (b-c):
 			ans+=1
 
 print(ans)
