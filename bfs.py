5b5fcf908e1285d9d2f5b42d72577e6b37f5101b
Seongjoo-Shin <ssj3357@naver.com>
2023-01-15 20:27:18 +0900

    0115 bfs, dfs, backtracking

---
+++ bfs.py
@@ -0,0 +1,17 @@
+# from collections import deque
+
+# adj = [[0] * 13 for _ in range(13)]
+# adj[0][1] = adj[0][2] = 1
+# adj[1][3] = adj[1][4] = 1
+
+# def bfs():
+#   dq = deque()
+#   dq.append(0) # 탐색 시작 root 노드
+#   while dq:
+#     now = dq.popleft()
+#     for next in range(13):
+#       if adj[now][next]:
+#         dq.append(next)
+
+# bfs() 
+
---
+++ dfs.py
@@ -0,0 +1,11 @@
+# # 인접 행렬
+# adj = [[0] * 13 for _ in range(13)]
+# adj[0][1] = adj[0][2] = 1
+# adj[1][3] = adj[1][4] = 1
+# def dfs(now_node):
+#   for next in range(13):
+#     if adj[now_node][next]:
+#       dfs(next)
+
+# dfs(0)
+
