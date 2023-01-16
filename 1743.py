38ec03458599bd0f9fc602e3f94b5a7432517f86
Seongjoo-Shin <ssj3357@naver.com>
2023-01-16 22:04:34 +0900

    1743

--- 1743.py
+++ 1743.py
@@ -1,30 +1,39 @@
 from collections import deque
 
+dy = (0, 1, 0, -1)
+dx = (1, 0, -1, 0)
+
+def is_valid_coord(y, x):
+	return 0 <= y < N and 0 <= x < M
+
+def bfs(start_y, start_x):
+	q = deque()
+	q.append((start_y, start_x))
+	res = 1
+	while len(q) > 0:
+		y, x = q.popleft()
+		for k in range(4):
+			ny = y + dy[k]
+			nx = x + dx[k]
+			if is_valid_coord(ny, nx) and adj[ny][nx] == 1:
+				q.append((ny, nx))
+				adj[ny][nx] = 0
+				res += 1
+	return res
+
 N, M, K = map(int, input().split())
 
-adj = [[0] * (M+1) for _ in range(N+1)]
-
+adj = [[0] * M for _ in range(N)]
 for _ in range(1, K+1):
-	a, b = map(int, input().split())
-	adj[a][b] = 1
+	y, x = map(int, input().split())
+	adj[y-1][x-1] = 1
 
-def bfs(v):
-	q = deque()
-	chk = [False] * (K+1)
-	chk[v] = True
-	q.append(v)
-	ans = 0
-	while q:
-		now = q.popleft()
-		for nxt in range(1, K):
-			if adj[now][nxt] == 1 and not chk[nxt]:
-				chk[nxt] = True
-				q.append(nxt)
-				ans += 1
-	return ans
 
 answer = []
-for i in range(1, K+1):
-	answer.append(bfs(i))
+for i in range(N):
+	for j in range(M):
+		if adj[i][j] == 1:
+			adj[i][j] = 0
+			answer.append(bfs(i, j))
 
-print(answer)
+print(max(answer))
