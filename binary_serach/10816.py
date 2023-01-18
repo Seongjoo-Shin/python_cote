ecb9320e9c76b3c15d40f1aeec3580116153e819
Shin Seongjoo <tloc0e04@osstem.com>
2023-01-18 09:51:43 +0900

    10816

---
+++ binary_serach/10816.py
@@ -0,0 +1,14 @@
+from bisect import bisect_left, bisect_right
+
+N = int(input())
+cards = sorted(list(map(int, input().split())))
+M = int(input())
+qry = list(map(int, input().split()))
+print(cards)
+ans = []
+for q in qry:
+	ll = bisect_left(cards, q)
+	rr = bisect_right(cards, q)
+	ans.append(rr-ll)
+
+print(*ans)
