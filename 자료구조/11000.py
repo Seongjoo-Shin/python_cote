80207edaca31264aabf14386d758cb3719ed9f37
Seongjoo-Shin <ssj3357@naver.com>
2023-01-24 22:58:53 +0900

    11000

---
+++ 자료구조/11000.py
@@ -0,0 +1,20 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+
+arr = []
+
+for _ in range(N):
+  a, b = map(int, input().split())
+  arr.append((a, b))
+
+q = [0]
+
+for a, b in sorted(arr):
+  if q[0] <= a:
+    heapq.heappop(q)
+  heapq.heappush(q, b)
+print(len(q))
