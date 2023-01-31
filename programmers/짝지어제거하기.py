e2b3ad6e4971f907cb626aadbb597c3b8921e169
Seongjoo-Shin <ssj3357@naver.com>
2023-01-31 10:53:48 +0900

    짝지어제거하기

---
+++ programmers/짝지어제거하기.py
@@ -0,0 +1,16 @@
+def solution(s):
+	stk = []
+	for i in s:
+		if len(stk) == 0:
+			stk.append(i)
+		elif stk[-1] == i:
+			stk.pop()
+		else:
+			stk.append(i)
+	if len(stk) == 0:
+		return 1
+	else:
+		return 0
+
+print(solution("baabaa"))
+print(solution("cdcd"))
