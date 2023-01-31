131f7272686d2c1589edfbd3a67d8bd0ea6b5a2d
Seongjoo-Shin <ssj3357@naver.com>
2023-01-31 09:53:39 +0900

    숫자의표현

--- programmers/숫자의표현.py
+++ programmers/숫자의표현.py
@@ -8,9 +8,25 @@
 # 4 + 5 + 6 = 15
 # 7 + 8 = 15
 # 15 = 15
-# 자연수 n이 매개변수로 주어질 때, 연속된 자연수들로 n을 표현하는
+# 자연수 n이 매개변수로 주어질 때, 
+
+# 연속된 자연수들로 n을 표현하는
+
 # 방법의 수를 return하는 solution를 완성해주세요.
 
 def solution(n):
 	answer = 0
+	for i in range(1,n+1):
+		sum = 0
+		for j in range(i, n+1):
+			sum += j
+			if sum == n:
+				answer += 1
+				break
+			elif sum > n:
+				break
+
 	return answer
+
+
+print(solution(15))
