74186c6a8d558912b452ed06adb43dba1e57cdd3
Seongjoo-Shin <ssj3357@naver.com>
2023-02-01 09:45:25 +0900

    최고의 집합

--- programmers/최고의집합.py
+++ programmers/최고의집합.py
@@ -1,36 +1,16 @@
-# 자연수 n 개로 이루어진 중복 집합(multi set, 편의상 이후에는 "집합"으로 통칭) 중에 
-
-# 다음 두 조건을 만족하는 집합을 최고의 집합이라고 합니다.
-
-# 각 원소의 합이 S가 되는 수의 집합
-# 위 조건을 만족하면서 각 원소의 곱 이 최대가 되는 집합
-# 예를 들어서 자연수 2개로 이루어진 집합 중 합이 9가 되는 집합은 다음과 같이 4개가 있습니다.
-# { 1, 8 }, { 2, 7 }, { 3, 6 }, { 4, 5 }
-# 그중 각 원소의 곱이 최대인 { 4, 5 }가 최고의 집합입니다.
-
-# 집합의 원소의 개수 n과 모든 원소들의 합 s가 매개변수로 주어질 때, 최고의 집합을 return 하는 solution 함수를 완성해주세요.
-from itertools import combinations, permutations
-
+# n개의 곱이 가장 큰 경우는 공평하게 나눈 값을 곱하거나 나머지를 더해서 곱하는 경우가 가장 크다는 아이디어에서 해결
 def solution(n, s):
 	answer = []
-	lst = [i for i in range(1, s+1)]
-	temp = 1
-	lst_set = list(combinations(lst, 2))
-	for a, b in lst_set:
-		if a+b == s:
-			chk = a * b
-			if temp < chk:
-				temp = chk
-				answer = [a, b]
-	if s % 2 == 0:
-		if temp < pow((s // 2), 2):
-			answer = [s//2, s//2]
-	
-	if not answer:
-		answer = [-1]
-	return answer
+	if n > s:
+		return [-1]
+	p, q = divmod(s, n)
+	answer = [p] * n
+
+	for i in range(q):
+		answer[i] += 1
+	return sorted(answer)
 
 
 print(solution(2, 9))
 print(solution(2, 1))
-print(solution(2, 8))
+print(solution(3, 8))
