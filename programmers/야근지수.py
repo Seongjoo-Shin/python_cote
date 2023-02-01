b954e75e14b416c063244762cd75271cc651eee6
Seongjoo-Shin <ssj3357@naver.com>
2023-02-01 11:03:14 +0900

    야근지수지

--- programmers/야근지수.py
+++ programmers/야근지수.py
@@ -1,20 +1,16 @@
 def solution(n, works):
-  answer = 0
-  works.sort(reverse=True)
-  cnt=n
-  while cnt:
-    if works[0]==0:
-      break
-    for i in range(len(works)):
-      if cnt==0 or works[0]>works[i]:
-        break
-      works[i]-=1
-      cnt-=1
+	if sum(works) <= n:
+		return 0
+	works.sort(reverse=True)
+	cnt=n
+	while cnt:
+		for i in range(len(works)):
+			if cnt==0 or works[0]>works[i]:
+				break
+			works[i]-=1
+			cnt-=1
 
-  for i in works:
-    answer+=i*i
-
-  return answer
+	return sum([w*w for w in works])
 
 print(solution(4, [4, 3, 3]))
 print(solution(1, [2, 1, 2]))
