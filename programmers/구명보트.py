07765a0c4b90e9f30d84f1b3f1b306a5f6b4f07d
Seongjoo-Shin <ssj3357@naver.com>
2023-02-02 14:15:06 +0900

    구명보트

--- programmers/구명보트.py
+++ programmers/구명보트.py
@@ -1,12 +1,15 @@
 def solution(people, limit):
 	answer = 0
 	people.sort()
-	while people:
-		for i in range(len(people)):
-			for j in range(2):
-					print(i, j)
 
-	return answer
+	start = 0
+	end = len(people) - 1
+	while start < end:
+		if people[start] + people[end] <= limit:
+			answer += 1
+			start += 1
+		end -= 1
+	return len(people) - answer
 
 print(solution([70, 50, 80, 50], 100))  # 3
 print(solution([70, 80, 50], 100))      # 3
