cf6e7e50ceabe2bc93d237367e435a794c49f4f7
Seongjoo-Shin <ssj3357@naver.com>
2023-02-05 22:07:45 +0900

    멀리뛰기

---
+++ programmers/멀리뛰기.py
@@ -0,0 +1,15 @@
+def solution(n):
+    dp = [0] * n
+    if n < 2:
+        return n
+    dp[0] = 1
+    dp[1] = 2
+
+    for i in range(2, n):
+        dp[i] = dp[i-1] + dp[i-2]
+    
+    print(dp)
+    return dp[-1] % 1234567
+
+print(solution(4)) # 5
+print(solution(3)) # 3
