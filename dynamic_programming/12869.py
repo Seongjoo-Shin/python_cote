b5f05faa5d5ff4bcf09c3eb9d19f8be55841c79a
Seongjoo-Shin <ssj3357@naver.com>
2023-02-07 13:53:02 +0900

    12869

---
+++ dynamic_programming/12869.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+scv = list(map(int, input().split()))
+mutal = [9, 3, 1]
+
+answer = 0
+while sum(scv) > 0:
+	scv.sort(reverse=True)
+	for i in range(len(scv)):
+		if scv[i] != 0:
+			scv[i] -= mutal[i]
+	if scv[-1] == 0:
+		scv.pop()
+	answer += 1
+
+print(answer)
