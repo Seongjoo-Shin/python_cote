1e1329a53b7b8afea5dc2c8822717473eec5fdbf
Seongjoo-Shin <ssj3357@naver.com>
2023-02-10 14:41:28 +0900

    12869

--- dynamic_programming/12869.py
+++ dynamic_programming/12869.py
@@ -1,15 +1,36 @@
+import sys
+from itertools import permutations
+
+input = sys.stdin.readline
+
+def answer(x, y, z, cnt):
+	global ans
+	if x <= 0 and y <= 0 and z <= 0:
+		if ans > cnt:
+			ans = cnt
+			return
+	
+	x = 0 if x <= 0 else x
+	y = 0 if y <= 0 else y
+	z = 0 if z <= 0 else z
+
+	if dp[x][y][z] <= cnt and dp[x][y][z] != 0:
+		return 
+
+	dp[x][y][z] = cnt
+	
+	for i in permutations([9, 3, 1], 3):
+		answer(x - i[0], y - i[1], z - i[2], cnt + 1)
+
 N = int(input())
 scv = list(map(int, input().split()))
-mutal = [9, 3, 1]
 
-answer = 0
-while sum(scv) > 0:
-	scv.sort(reverse=True)
-	for i in range(len(scv)):
-		if scv[i] != 0:
-			scv[i] -= mutal[i]
-	if scv[-1] <= 0:
-		scv.pop()
-	answer += 1
+while len(scv) < 3:
+	scv += [0]
 
-print(answer)
+ans = 100
+
+dp = [[[100] * (max(scv) + 1) for i in range((max(scv) + 1))] for j in range((max(scv) + 1))]
+answer(scv[0], scv[1], scv[2], 0)
+# print(dp)
+print(ans)
