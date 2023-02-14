274d949c7a29195f6d272176fe2b1843d5e52c52
Seongjoo-Shin <ssj3357@naver.com>
2023-02-14 09:40:16 +0900

    ;;

--- brute_force/10819.py
+++ brute_force/10819.py
@@ -3,31 +3,37 @@ lst = list(map(int, input().split()))
 
 lst.sort()
 
-ans = 0
+
+def aa(lst, n):
+	ans = 0
+	s = lst[:n//2]
+	b = lst[n//2:]
+	aa = []
+	for i in range(n//2-1, -1, -1):
+		aa.append(s[i])
+		aa.append(b[i])
+
+	for i in range(1, n):
+		ans += abs(aa[i-1] - aa[i])
+	print(aa)
+	return ans
+
+def bb(lst, n):
+	ans = 0
+	s = lst[:n//2+1]
+	b = lst[n//2+1:]
+	aa = []
+	for i in range(n//2, -1, -1):
+		aa.append(s[i])
+		if i != 0:
+			aa.append(b[i-1])
+
+	for i in range(1, n):
+		ans += abs(aa[i-1] - aa[i])
+	print(aa)
+	return ans
 
 if n % 2 == 0:
-    s = lst[:n//2]
-    b = lst[n//2:]
-    aa = []
-    for i in range(n//2-1, -1, -1):
-        aa.append(s[i])
-        aa.append(b[i])
-    for i in range(1, n):
-        ans += abs(aa[i-1] - aa[i])
-else : 
-    s = lst[:n//2+1]
-    b = lst[n//2+1:]
-    aa = []
-    for i in range(n//2, -1, -1):
-        print(i)
-        aa.append(s[i])
-        if i != 0:
-            aa.append(b[i-1])
-    for i in range(1, n):
-        ans += abs(aa[i-1] - aa[i])
-
-
-print(ans)
-# 8 20 4 15 1 
-# 
-# 
+	print(aa(lst, n))
+else:
+	print(bb(lst, n))
