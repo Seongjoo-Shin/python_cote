ac37f75bdbe630d90624da48beb66f39df169b02
Seongjoo-Shin <ssj3357@naver.com>
2023-02-20 18:00:01 +0900

    2667

--- bfs_dfs/2667.py
+++ bfs_dfs/2667.py
@@ -1,7 +1,4 @@
 from collections import deque
-import sys
-
-input = sys.stdin.readline
 
 dy = [0, 1, 0, -1]
 dx = [1, 0, -1, 0]
@@ -14,33 +11,27 @@ visited = [[False] * n for _ in range(n)]
 def bfs(y, x):
 	q = deque()
 	q.append((y, x))
-	visited[y][x] == True
+	m[y][x] = '0'
 	cnt = 0
 	while q:
+		cnt += 1
 		y, x = q.popleft()
-
 		for i in range(4):
 			ny = y + dy[i]
 			nx = x + dx[i]
 
-			if 0 <= ny < n and 0 <= nx < n and not visited[ny][nx] and m[ny][nx] == '1':
-				visited[ny][nx] = True
-				q.append((ny, nx))
-				cnt += 1
+			if 0 <= ny < n and 0 <= nx < n and m[ny][nx] == '1':
+				m[ny][nx] = '0'
+				q.append((ny, nx))		
 	return cnt
+
 
 ans = []
 for i in range(n):
 	for j in range(n):
-		if m[i][j] == '1' and not visited[i][j]:
+		if m[i][j] == '1' :
 			ans.append(bfs(i, j))
 
 print(len(ans))
 for i in ans:
 	print(i)
-
-print()
-
-for r in visited:
-	print(r)
-	
