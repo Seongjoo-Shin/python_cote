2b414147849318df0a6c9a755ae8093d692d3dd9
Seongjoo-Shin <ssj3357@naver.com>
2023-02-22 17:27:30 +0900

    네트워크

--- programmers/네트워크.py
+++ programmers/네트워크.py
@@ -1,6 +1,34 @@
+from collections import deque
+
 def solution(n, computers):
-    answer = 0
-    return answer
+	answer = 0
+	graph = {}
+	for start in range(n):
+		graph[start] = []
+		for end,edge in enumerate(computers[start]):
+			if edge == 1 and start!=end :
+				graph[start].append(end)
+	visited=[]
+	not_visited =list(graph.keys())
+	print('graph : ', graph)
+	print('not_visited : ', not_visited)
+	while not_visited :
+		visited += bfs(graph , not_visited [0])
+		not_visited = list(set(graph.keys()) - set(visited))
+		answer+=1
+	return answer
+
+def bfs(graph, root):
+	visited = []
+	q = deque([root])
+
+	while q:
+		now = q.popleft()
+		if now not in visited:
+			visited.append(now)
+			q += list(set(graph[now]) - set(visited))
+	return visited
+	
 
 
 print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])) # 2
