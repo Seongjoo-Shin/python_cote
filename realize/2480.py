6e35a0b69c7310a0f386c4b748aa8dc8fc1c541b
Seongjoo-Shin <ssj3357@naver.com>
2023-02-27 22:34:27 +0900

    2480

--- realize/2480.py
+++ realize/2480.py
@@ -1,14 +1,10 @@
 a, b, c = map(int, input().split())
 
-result = 0
-
-if a == b and b == c and c == a:
-    result = 10000 + a * 1000
+if a == b == c:
+    print(10000 + a * 1000)
 elif a == b or a == c:
-    result = 1000 + a * 100
+    print(1000 + a * 100)
 elif b == c:
-    result = 100 + b * 100
+    print(1000 + b * 100)
 else:
-    result = max(a, max(b, c)) * 100
-
-print(result)
+    print(max(a, b, c) * 100)
